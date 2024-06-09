503cab67ec85be76a270edc81d8bb822db421534
preferrrr <zzangsh2946@naver.com>
2024-06-10 04:51:36 +0900

    feat : 로그인 비즈니스 로직 추가 단위 함수 추가

--- backend/src/main/java/com/light/backend/member/service/MemberServiceSupport.java
+++ backend/src/main/java/com/light/backend/member/service/MemberServiceSupport.java
@@ -6,8 +6,12 @@ import com.light.backend.member.domain.MemberRole;
 import com.light.backend.member.exception.*;
 import com.light.backend.member.repository.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 import static com.light.backend.member.domain.MemberRole.*;
 
@@ -18,6 +22,10 @@ public class MemberServiceSupport {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String REFRESH_TOKEN = "refreshToken";
 
     public Member getMemberById(String id) {
         return memberRepository.findById(id).orElseThrow(NotFoundMemberException::new);
@@ -41,5 +49,35 @@ public class MemberServiceSupport {
     public void save(Member member) {
         memberRepository.save(member);
     }
+
+    public void checkPassword(String requestPassword, String originPassword) {
+        if (!passwordEncoder.matches(requestPassword, originPassword))
+            throw new NotMatchPasswordException();
+    }
+
+    public String createRefreshTokenValue() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    public HttpHeaders createJwt(String email, MemberRole role, String refreshTokenValue) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(AUTHORIZATION, jwtProvider.createAccessToken(email, role)); // access token
+
+        ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN, jwtProvider.createRefreshToken(refreshTokenValue))
+                .maxAge(14 * 24 * 60 * 60)
+                .path("/")
+                .secure(true)
+                .sameSite("None")
+                .httpOnly(true)
+                .build();
+
+        headers.add(HttpHeaders.COOKIE, cookie.toString()); // refresh token
+
+        return headers;
+    }
+
+
+
 
 }
