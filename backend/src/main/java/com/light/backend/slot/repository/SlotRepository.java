9e1f8591d477716cc7d101bf365c844f375c1891
preferrrr <zzangsh2946@naver.com>
2024-06-11 01:04:18 +0900

    feat : SlotRepository 추가

---
+++ backend/src/main/java/com/light/backend/slot/repository/SlotRepository.java
@@ -0,0 +1,9 @@
+package com.light.backend.slot.repository;
+
+import com.light.backend.slot.domain.Slot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SlotRepository extends JpaRepository<Slot, Long> {
+}
