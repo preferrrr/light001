78ad2a7ba53cbf8bc566c8b518d9f73085a000d9
preferrrr <zzangsh2946@naver.com>
2024-06-11 21:52:38 +0900

    feat : slot 데이터 설정 비즈니스 로직 구성 함수 추가

--- backend/src/main/java/com/light/backend/slot/service/SlotServiceSupport.java
+++ backend/src/main/java/com/light/backend/slot/service/SlotServiceSupport.java
@@ -1,9 +1,12 @@
 package com.light.backend.slot.service;
 
+import com.light.backend.member.domain.Member;
 import com.light.backend.member.domain.MemberRole;
 import com.light.backend.slot.domain.Slot;
 import com.light.backend.slot.exception.NotCreatedByAdminException;
+import com.light.backend.slot.exception.NotFoundSlotException;
 import com.light.backend.slot.exception.UnauthorizedCreateSlotException;
+import com.light.backend.slot.exception.UnauthorizedSetSlotDataException;
 import com.light.backend.slot.repository.SlotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,6 +31,16 @@ public class SlotServiceSupport {
     public void checkCreatedByAdmin(String createdBy, String adminId) {
         if (!createdBy.equals(adminId)) {
             throw new NotCreatedByAdminException();
+        }
+    }
+
+    public Slot getSlotById(Long id) {
+        return slotRepository.findById(id).orElseThrow(NotFoundSlotException::new);
+    }
+
+    public void checkSetSlotDataAuthority(Member currentMember, Member slotOwner) {
+        if (!currentMember.getRole().equals(MemberRole.MASTER) && slotOwner.getCreatedBy().getId() != currentMember.getId() && slotOwner.getId() != currentMember.getId()) {
+            throw new UnauthorizedSetSlotDataException();
         }
     }
 }
