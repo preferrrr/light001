949534ed7bef2204dd9bb51d279c6063b2537e92
preferrrr <zzangsh2946@naver.com>
2024-06-11 21:51:36 +0900

    refact : slot 데이터 설정 비즈니스 로직 추가

--- backend/src/main/java/com/light/backend/slot/service/SlotService.java
+++ backend/src/main/java/com/light/backend/slot/service/SlotService.java
@@ -4,6 +4,7 @@ import com.light.backend.global.utils.CurrentMemberGetter;
 import com.light.backend.member.domain.Member;
 import com.light.backend.member.service.MemberServiceSupport;
 import com.light.backend.slot.controller.dto.request.CreateSlotRequest;
+import com.light.backend.slot.controller.dto.request.SetSlotDataRequest;
 import com.light.backend.slot.domain.Slot;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class SlotService {
     private final MemberServiceSupport memberServiceSupport;
     private final CurrentMemberGetter currentMemberGetter;
 
+    @Transactional(readOnly = false)
     public void createSlot(CreateSlotRequest request) {
 
         //생성시켜줄 관리자
@@ -35,6 +37,27 @@ public class SlotService {
         Slot slot = request.toEntity(member);
 
         slotServiceSupport.save(slot);
+
+    }
+
+    @Transactional(readOnly = false)
+    public void setSlotData(SetSlotDataRequest request) {
+
+        //현재 멤버
+        Member currentMember = memberServiceSupport.getMemberById(currentMemberGetter.getCurrentMemberId());
+
+        //데이터를 저장할 슬롯
+        Slot slot = slotServiceSupport.getSlotById(request.getId());
+
+        //슬롯의 데이터를 수정할 수 있는지 권한 확인
+        slotServiceSupport.checkSetSlotDataAuthority(currentMember, slot.getOwner());
+
+        //슬롯 데이터 수정
+        slot.setData(request);
+
+        boolean isError = false; //네이버 open api 호출
+
+        slot.updateErrorState(isError);
 
     }
 
