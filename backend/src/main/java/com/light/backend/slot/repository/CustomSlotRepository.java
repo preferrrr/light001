f5526eecba26fed102de28c523035e42dfabc5b2
preferrrr <zzangsh2946@naver.com>
2024-06-14 18:04:57 +0900

    refact : 대시보드(슬롯 현황 조회) 동적 쿼리 추가

--- backend/src/main/java/com/light/backend/slot/repository/CustomSlotRepository.java
+++ backend/src/main/java/com/light/backend/slot/repository/CustomSlotRepository.java
@@ -1,14 +1,18 @@
 package com.light.backend.slot.repository;
 
 import com.light.backend.member.domain.Member;
+import com.light.backend.slot.controller.dto.response.GetDashboardResponse;
 import com.light.backend.slot.controller.dto.response.SearchSlotResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface CustomSlotRepository {
 
     Page<SearchSlotResponse> findSlotByTypeAndValue(Member member, String type, String value, Pageable pageable);
+    GetDashboardResponse getDashboard(Member member, LocalDate now);
 
 }
--- backend/src/main/java/com/light/backend/slot/repository/CustomSlotRepositoryImpl.java
+++ backend/src/main/java/com/light/backend/slot/repository/CustomSlotRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.light.backend.slot.repository;
 
 import com.light.backend.member.domain.Member;
-import com.light.backend.member.domain.MemberRole;
-import com.light.backend.member.domain.QMember;
+import com.light.backend.slot.controller.dto.response.GetDashboardResponse;
+import com.light.backend.slot.controller.dto.response.QGetDashboardResponse;
 import com.light.backend.slot.controller.dto.response.SearchSlotResponse;
 import com.light.backend.slot.domain.QSlot;
 import com.light.backend.slot.domain.Slot;
+import com.light.backend.slot.domain.SlotErrorState;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,9 +18,11 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import static com.light.backend.member.domain.MemberRole.*;
+import static com.light.backend.member.domain.MemberRole.ADMIN;
+import static com.light.backend.member.domain.MemberRole.MASTER;
 
 @Repository
 @RequiredArgsConstructor
@@ -55,6 +61,65 @@ public class CustomSlotRepositoryImpl implements CustomSlotRepository {
                 count
         );
     }
+
+    @Override
+    public GetDashboardResponse getDashboard(Member currentMember, LocalDate now) {
+
+        return jpaQueryFactory
+                .select(new QGetDashboardResponse(
+                                slot.count().intValue(),
+                                waitingSlot(),
+                                runningSlot(now),
+                                expiringSlot(now),
+                                closedSlot(now),
+                                errorSlot(now)
+                        )
+                )
+                .from(slot)
+                .where(memberCondition(currentMember))
+                .fetchOne();
+    }
+
+    private NumberExpression<Integer> waitingSlot() {
+        return new CaseBuilder()
+                .when(slot.mid.isNull())
+                .then(1)
+                .otherwise(0)
+                .sum();
+    }
+
+    private NumberExpression<Integer> runningSlot(LocalDate now) {
+        return new CaseBuilder()
+                .when(slot.startAt.loe(now).and(slot.endAt.goe(now)))
+                .then(1)
+                .otherwise(0)
+                .sum();
+    }
+
+    private NumberExpression<Integer> closedSlot(LocalDate now) {
+        return new CaseBuilder()
+                .when(slot.endAt.eq(now.minusDays(1)))
+                .then(1)
+                .otherwise(0)
+                .sum();
+    }
+
+    private NumberExpression<Integer> expiringSlot(LocalDate now) {
+        return new CaseBuilder()
+                .when(slot.endAt.between(now, now.plusDays(2)))
+                .then(1)
+                .otherwise(0)
+                .sum();
+    }
+
+    private NumberExpression<Integer> errorSlot(LocalDate now) {
+        return new CaseBuilder()
+                .when(slot.startAt.loe(now).and(slot.endAt.goe(now)).and(slot.slotErrorState.eq(SlotErrorState.Y)))
+                .then(1)
+                .otherwise(0)
+                .sum();
+    }
+
 
     private BooleanExpression memberCondition(Member member) {
         if (member.getRole().equals(MASTER))
