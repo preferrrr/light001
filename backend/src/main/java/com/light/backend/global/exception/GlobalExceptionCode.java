0fa9940effd3eecb401c0b6eb95cd068344994e0
preferrrr <zzangsh2946@naver.com>
2024-06-16 03:32:39 +0900

    feat : Bind Exception 처리 추가

--- backend/src/main/java/com/light/backend/global/exception/GlobalExceptionCode.java
+++ backend/src/main/java/com/light/backend/global/exception/GlobalExceptionCode.java
@@ -4,10 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 @Getter
 @RequiredArgsConstructor
 public enum GlobalExceptionCode implements ExceptionCode {
-    ;
+
+    INVALID_REQUEST_PARAMETER(BAD_REQUEST, "G001", "유효하지 않은 파라미터 입니다.");;
 
     private final HttpStatus httpStatus;
     private final String errorCode;
--- backend/src/main/java/com/light/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/light/backend/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,27 @@
 package com.light.backend.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.light.backend.global.exception.GlobalExceptionCode.*;
 
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ExceptionResponse> handleBindException(final BindException e) {
+
+        String errorMessage = e.getAllErrors().get(0).getDefaultMessage();
+        log.error("[bind exception] {}", errorMessage);
+
+        return new ResponseEntity<>(
+                ExceptionResponse.of(INVALID_REQUEST_PARAMETER, errorMessage),
+                INVALID_REQUEST_PARAMETER.getHttpStatus()
+        );
+    }
 }
